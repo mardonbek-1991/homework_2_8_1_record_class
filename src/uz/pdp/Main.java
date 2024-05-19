2778b5215b43639135eccc7a3ca07df4c1bf0504
Mardonbek Yusupov <mardonbek.yusupov.91@mail.ru>
2024-05-19 12:03:50 +0500

    The name of project is changed

--- src/uz/pdp/Main.java
+++ src/uz/pdp/Main.java
@@ -1,2 +1,21 @@
-package uz.pdp;public class Main {
+package uz.pdp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        RecordClass recordClass = new RecordClass("How to make friends", "Deyl Karnegy", 150.5);
+        System.out.println(recordClass);
+
+        RecordClass recordClass2 = new RecordClass(null, "Deyl Karnegy", 150.5);
+        System.out.println(recordClass2);
+
+        RecordClass recordClass3 = new RecordClass("", "Deyl Karnegy", 150.5);
+        System.out.println(recordClass3);
+
+        RecordClass recordClass4 = new RecordClass("How to make friends", "Deyl Karnegy", -150.5);
+        System.out.println(recordClass4);
+
+
+    }
+
 }
--- src/uz/pdp/RecordClass.java
+++ src/uz/pdp/RecordClass.java
@@ -1,2 +1,16 @@
-package uz.pdp;public record RecordClass() {
+package uz.pdp;
+
+public record RecordClass(String name, String author, double priceBook) {
+
+    public RecordClass {
+
+        if (name == null || author == null) {
+            throw new IllegalArgumentException("The field is null");
+        } else if (name.isEmpty() || author.isEmpty()) {
+            throw new IllegalArgumentException("The field is empty");
+        } else if (priceBook < 0) {
+            throw new IllegalArgumentException("Price Book can not be negative number");
+        }
+
+    }
 }
